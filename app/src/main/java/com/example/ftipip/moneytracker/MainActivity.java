7b89158d6b0e33843abd89cb322231c31314f5da
Semen Grigorev <ftip@list.ru>
2015-08-26 16:58:13 +0300

    Logging message added

--- app/src/main/java/com/example/ftipip/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/ftipip/moneytracker/MainActivity.java
@@ -2,22 +2,28 @@ package com.example.ftipip.moneytracker;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, "onCreate");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
