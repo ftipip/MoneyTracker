c2a882dde61e6f76887cafd1add0740af024d92c
Semen Grigorev <ftip@list.ru>
2015-08-28 21:02:39 +0300

    ListView

--- app/src/main/java/com/example/ftipip/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/ftipip/moneytracker/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView listView;
+    private List<Transaction> data = new ArrayList<>();
+    private TransactionAdapter transactionAdapter;
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -16,7 +25,19 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listView = (ListView) findViewById(R.id.main_listview);
+        List<Transaction> adapterData = getDataList();
+        transactionAdapter = new TransactionAdapter(this, adapterData);
+        listView.setAdapter(transactionAdapter);
+
         Log.d(LOG_TAG, "onCreate");
+    }
+
+    private  List<Transaction> getDataList() {
+        data.add(new Transaction("Phone", 2000, String.valueOf(new Date())));
+        data.add(new Transaction("Phone", 3000, String.valueOf(new Date())));
+
+        return data;
     }
 
     @Override
